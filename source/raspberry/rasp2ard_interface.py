badd9b7c429406d389f72d44554ac7eb66decb8b
Malvaso <73135597+Malvaso@users.noreply.github.com>
2024-05-21 17:46:09 -0300

    intento de peticion (masomenos fallido)
    trate de pedirle una sola vez el dato de la clasifcacion a la raspberry pero no funciona tan bien, me lo da 3 veces.
    Igual lo subo pq tambien le modifique otras cosas, como el rango de colores en HSV segun la luz, el ancho de la ventana de deteccion y el contorno minimo a detectar.

--- source/raspberry/rasp2ard_interface.py
+++ source/raspberry/rasp2ard_interface.py
@@ -10,11 +10,18 @@ pip install pyserial
 """
 
 # Defino las macros para forma y color
-OBJ_INVALIDO = b'0'
-CUBO_VERDE   = b'1'
-ESFERA_VERDE = b'2'
-CUBO_ROJO    = b'3'
-ESFERA_ROJA  = b'4'
+CUBO = 0x30         # '0'
+ESFERA = 0x31       # '1'
+VERDE = 0x01        #  1
+ROJO = 0x03         #  3
+
+"""
+Segun los valores ASCII elegidos, las combinaciones quedan como sigue:
+* CUBO VERDE = '1'
+* ESF. VERDE = '2'
+* CUBO ROJO  = '3'
+* ESF. ROJA  = '4'
+"""
 
 # Importo las bibliotecas necesarias
 import serial
@@ -28,25 +35,27 @@ seri.flushInput()
 # Funcion para detectar el objeto
 def codigoObjeto(approxPoly, color):
     # Inicializo el byte a transmitir
-    objeto = OBJ_INVALIDO
+    objeto = 0
 
     if len(approxPoly) <= 9 :
         if color == (0,255,0):
-            objeto = CUBO_VERDE
-        elif color == (0,0,255):
-            objeto = CUBO_ROJO
-
+            objeto = 1
+        if color == (0,0,255):
+            objeto = 3
+        
     if len(approxPoly) >= 11:
         if color == (0,255,0):
-            objeto = ESFERA_VERDE
-        elif color == (0,0,255):
-            objeto = ESFERA_ROJA
+            objeto = 2
+        if color == (0,0,255):
+            objeto = 4
+
     # Verifico que el objeto se haya detectado correctamente
-    if not (objeto == CUBO_VERDE or
-            objeto == CUBO_ROJO  or
-            objeto == ESFERA_VERDE or
-            objeto == ESFERA_ROJA):
-        objeto = OBJ_INVALIDO
+    """
+    Es horrible no poder intercambiar entre chars y bytes indistintamente
+    pero Python no me deja. Basicamente necesito que objeto este entre
+    '1' y '4' pero sin ser de tipo char.
+    """
+    #if 0x31 <= objeto <= 0x34:  objeto = 0
     
     return objeto
 
@@ -55,7 +64,7 @@ def dibujar(mask, color):
     contornos, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for c in contornos:
         area = cv2.contourArea(c)
-        if area > 500:
+        if area > 20000:
             M = cv2.moments(c)
             if (M["m00"]==0): M["m00"] = 1
             x = int(M["m10"]/M["m00"])
@@ -73,50 +82,86 @@ def dibujar(mask, color):
 
             # Envio el codigo del objeto seleccionado al Arduino
             objeto = codigoObjeto(approx, color)
-            seri.write(objeto)
-
+            
+            objetostr = str(objeto)
+            objetostr +="\n"
+            mensaje=objetostr.encode(encoding='UTF-8',errors='strict')
+            
+            lectura=seri.readline()
+            trigger=lectura.decode('UTF-8').strip()
+            if trigger=="leer":
+	            seri.write(mensaje)
+	            print(mensaje)
+            
+            #lectura=seri.readline()
+            #trigger=lectura.decode('UTF-8').strip()
+            #if(trigger=="leer"):
+            
+            #seri.write(mensaje)
+			#
+			
+			            
+            
+            #, "   y en string:  ",objetostr)
+            
             # A partir de aca es codigo de compatibilidad para dibujar y escribir en el stream de video
             # TODO: Borrar en la final release
-            if objeto != OBJ_INVALIDO:
-                if objeto == CUBO_VERDE or objeto == CUBO_ROJO:
-                    shape_name = "CUBO"
-                else:
-                    shape_name = "ESFERA"
-                
-                if objeto == CUBO_VERDE or objeto == ESFERA_VERDE:
-                    color_name = "VERDE"
-                else:
-                    color_name = "ROJO"
-
-                cv2.drawContours(frame, [nuevoContorno], 0, color, 3)
-                cv2.putText(frame, color_name, (x+10,y), font, 0.75, color, 1, cv2.LINE_AA)
-                cv2.putText(frame, shape_name, (x+10,y-20), font, 0.75, color, 1, cv2.LINE_AA)
+            #obj = objeto - 0x30
+            
+            if objeto == 1:   
+                color_name = "verde"  
+                shape_name = "CUBO"
+            
+            if objeto == 2:   
+                color_name = "verde"  
+                shape_name = "esfera"
+            
+            if objeto == 3:   
+                color_name = "rojo"  
+                shape_name = "CUBO"
+            
+            if objeto == 4:   
+                color_name = "roja"  
+                shape_name = "esfera"
+            
+            if objeto < 1 or objeto >4:
+                color_name = "NADA"  
+                shape_name = "nada"
+            
+            cv2.drawContours(frameobjeto, [nuevoContorno], 0, color, 3)
+            cv2.putText(frameobjeto, color_name, (x+10,y), font, 0.75, color, 1, cv2.LINE_AA)
+            cv2.putText(frameobjeto, shape_name, (x+10,y-20), font, 0.75, color, 1, cv2.LINE_AA)
             
 cap = cv2.VideoCapture(0)
 
-verdeBajo = np.array([40,50,10],np.uint8)
+verdeBajo = np.array([40,100,20],np.uint8)
 verdeAlto = np.array([80,255,255],np.uint8)
 
-redBajo1 = np.array([0,100,20],np.uint8)
+redBajo1 = np.array([0,180,50],np.uint8)
 redAlto1 = np.array([15,255,255],np.uint8)
 
-redBajo2 = np.array([170,100,20],np.uint8)
+redBajo2 = np.array([170,180,50],np.uint8)
 redAlto2 = np.array([179,255,255],np.uint8)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 while True:
 
   ret,frame = cap.read()
+  x1,y1=500,200
+  x2,y2=1800,900
+  zonadeteccion=frame.copy()
+  frameobjeto= zonadeteccion[y1:y2,x1:x2]   # hago mas chica el area de deteccion, en realidad el area que cubre la camara es mayor
+	
 
   if ret == True:
-    frameHSV = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    frameHSV = cv2.cvtColor(frameobjeto,cv2.COLOR_BGR2HSV)
     maskVerde = cv2.inRange(frameHSV,verdeBajo,verdeAlto)
     maskRed1 = cv2.inRange(frameHSV,redBajo1,redAlto1)
     maskRed2 = cv2.inRange(frameHSV,redBajo2,redAlto2)
     maskRed = cv2.add(maskRed1,maskRed2)
     dibujar(maskVerde,(0,255,0))
     dibujar(maskRed,(0,0,255))
-    cv2.imshow('frame',frame)
+    cv2.imshow('camara',frameobjeto)
 
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
