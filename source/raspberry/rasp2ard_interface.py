9362adf89292cb9899ef5c932182c2eb0d214ca6
Malvaso <73135597+Malvaso@users.noreply.github.com>
2024-05-23 23:38:21 -0300

    peticion unica con delay
    solucione el problema de que mandaba muchas veces el mismo objeto, con el viejo truco del delay (con millis). El ciclo tarda un segundo en completarse, me parece un tiempo razonable

--- source/raspberry/rasp2ard_interface.py
+++ source/raspberry/rasp2ard_interface.py
@@ -37,13 +37,13 @@ def codigoObjeto(approxPoly, color):
     # Inicializo el byte a transmitir
     objeto = 0
 
-    if len(approxPoly) <= 9 :
+    if len(approxPoly) <= 7 :
         if color == (0,255,0):
             objeto = 1
         if color == (0,0,255):
             objeto = 3
         
-    if len(approxPoly) >= 11:
+    if len(approxPoly) >= 10:
         if color == (0,255,0):
             objeto = 2
         if color == (0,0,255):
@@ -64,7 +64,7 @@ def dibujar(mask, color):
     contornos, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for c in contornos:
         area = cv2.contourArea(c)
-        if area > 20000:
+        if area > 70000:
             M = cv2.moments(c)
             if (M["m00"]==0): M["m00"] = 1
             x = int(M["m10"]/M["m00"])
@@ -87,12 +87,10 @@ def dibujar(mask, color):
             objetostr +="\n"
             mensaje=objetostr.encode(encoding='UTF-8',errors='strict')
             
-            lectura=seri.readline()
-            trigger=lectura.decode('UTF-8').strip()
             if trigger=="leer":
-	            seri.write(mensaje)
-	            print(mensaje)
-            
+                seri.write(mensaje)
+                print(mensaje)
+        
             #lectura=seri.readline()
             #trigger=lectura.decode('UTF-8').strip()
             #if(trigger=="leer"):
@@ -134,38 +132,57 @@ def dibujar(mask, color):
             
 cap = cv2.VideoCapture(0)
 
-verdeBajo = np.array([40,100,20],np.uint8)
+verdeBajo = np.array([40,80,20],np.uint8)
 verdeAlto = np.array([80,255,255],np.uint8)
 
-redBajo1 = np.array([0,180,50],np.uint8)
+redBajo1 = np.array([0,170,60],np.uint8)
 redAlto1 = np.array([15,255,255],np.uint8)
 
-redBajo2 = np.array([170,180,50],np.uint8)
+redBajo2 = np.array([170,170,60],np.uint8)
 redAlto2 = np.array([179,255,255],np.uint8)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
+
+
+
+
+
+
+
+
+
 while True:
+    
+    
 
-  ret,frame = cap.read()
-  x1,y1=500,200
-  x2,y2=1800,900
-  zonadeteccion=frame.copy()
-  frameobjeto= zonadeteccion[y1:y2,x1:x2]   # hago mas chica el area de deteccion, en realidad el area que cubre la camara es mayor
-	
+    ret,frame = cap.read()
+    x1,y1=500,200
+    x2,y2=1800,900
+    zonadeteccion=frame.copy()
+    frameobjeto= zonadeteccion[y1:y2,x1:x2]   # hago mas chica el area de deteccion, en realidad el area que cubre la camara es mayor
+    
+    
+    if ret == True:
+        frameHSV = cv2.cvtColor(frameobjeto,cv2.COLOR_BGR2HSV)
+        maskVerde = cv2.inRange(frameHSV,verdeBajo,verdeAlto)
+        maskRed1 = cv2.inRange(frameHSV,redBajo1,redAlto1)
+        maskRed2 = cv2.inRange(frameHSV,redBajo2,redAlto2)
+        maskRed = cv2.add(maskRed1,maskRed2)
+            
+        lectura=seri.readline()
+        trigger=lectura.decode('UTF-8').strip()
+        dibujar(maskVerde,(0,255,0))
+        dibujar(maskRed,(0,0,255))
+        
+        cv2.imshow('camara',frameobjeto)
+        #print(trigger)
+        
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
 
-  if ret == True:
-    frameHSV = cv2.cvtColor(frameobjeto,cv2.COLOR_BGR2HSV)
-    maskVerde = cv2.inRange(frameHSV,verdeBajo,verdeAlto)
-    maskRed1 = cv2.inRange(frameHSV,redBajo1,redAlto1)
-    maskRed2 = cv2.inRange(frameHSV,redBajo2,redAlto2)
-    maskRed = cv2.add(maskRed1,maskRed2)
-    dibujar(maskVerde,(0,255,0))
-    dibujar(maskRed,(0,0,255))
-    cv2.imshow('camara',frameobjeto)
 
-    k = cv2.waitKey(1) & 0xFF
-    if k == 27:
-        break
+
 
 cap.release()
 cv2.destroyAllWindows()
